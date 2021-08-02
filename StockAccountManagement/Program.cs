91d4b16f15a12470d9d6adadf7a37f35cc8e3055
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-02 18:57:27 +0530

    [Bharath] Add: Stock Report Method Implemented

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using StockAccountManagement.StockManagement;
+using System;
+
+namespace StockAccountManagement
+{
+    class Program
+    {
+        const string STOCK_JSON = @"C:\Users\USER\source\repos\StockAccountManagement\StockManagement\stock.json";
+        static void Main(string[] args)
+        {
+            StockPortfolio main = new StockPortfolio();
+            main.StockReport(STOCK_JSON);
+        }
+    }
+}
---
+++ StockAccountManagement/StockManagement/StockModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockAccountManagement.StockManagement
+{
+    class StockModel
+    {
+        public string StockName { get; set; }
+        public double Shares { get; set; }
+        public double SharePrice { get; set; }
+    }
+}
